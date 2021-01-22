09393e893c6822f3410e219c3431dcee99603ab2
Emre Aydın <gamertremre@gmail.com>
2021-01-22 22:54:10 +0300

    20 Ocak Ders Sonu

--- DegerveReferansTıpler/Program.cs
+++ DegerveReferansTıpler/Program.cs
@@ -17,7 +17,7 @@ namespace DegerveReferansTıpler
             int[] sayilar1 = new int[] { 10, 20, 30 };
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
-            sayilar2[0]=999:
+            sayilar2[0] = 999;
                 //sayilar1[0] = ?   999
 
 
